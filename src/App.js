55b2ffaf7ef630875652947dddd348b2e1a3a2d8
Kae Sluder <kaesluder@fastmail.com>
2022-12-19 22:06:15 -0500

    task list integration: read all, mark read

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import TaskList from './components/TaskList.js';
 import './App.css';
+import axios from 'axios';
 
 const TASKS = [
   {
@@ -14,6 +15,15 @@ const TASKS = [
     isComplete: true,
   },
 ];
+
+const TASKS_HOST = 'http://localhost:5000/';
+const TASKS_LIST_ENDPOINT = `${TASKS_HOST}tasks`;
+
+const markCompleteEndpoint = (id) =>
+  `${TASKS_LIST_ENDPOINT}/${id}/mark_complete`;
+const markIncompleteEndpoint = (id) =>
+  `${TASKS_LIST_ENDPOINT}/${id}/mark_incomplete`;
+const deleteEndpoint = (id) => `${TASKS_LIST_ENDPOINT}/${id}`;
 
 // Deletes items from task list matching id.
 const deleteTaskById = function (taskList, id) {
@@ -38,11 +48,53 @@ const toggleCompleteOnTask = (task) => {
   return { ...task, isComplete: !task.isComplete };
 };
 
+const cleanDataFromAPI = (task) => {
+  return {
+    id: task.id,
+    description: task.description,
+    isComplete: task.is_complete,
+    title: task.title,
+  };
+};
+
+const cleanDataToAPI = (task) => {
+  return {
+    description: task.description,
+    title: task.title,
+  };
+};
+
+const getTasksData = function () {
+  return axios
+    .get(TASKS_LIST_ENDPOINT)
+    .then((response) => {
+      console.log(response.data);
+      return response.data;
+    })
+    .catch((error) => console.log(error));
+};
+
 const App = () => {
   const [tasksData, setTasksData] = useState(TASKS);
 
+  useEffect(() => {
+    return getTasksData().then((data) =>
+      setTasksData(data.map(cleanDataFromAPI))
+    );
+  }, []);
+
   // Sets the complete status on a single task and modifies state.
   const setTasksHandler = (id) => {
+    // get a handle for the changed task outside of the
+    // map call.
+    const localTask = tasksData.filter((t) => t.id === id)[0];
+
+    if (localTask.isComplete) {
+      axios.patch(markIncompleteEndpoint(localTask.id));
+    } else {
+      axios.patch(markCompleteEndpoint(localTask.id));
+    }
+
     setTasksData((tasksData) =>
       mapMutate(tasksData, (task) => task.id === id, toggleCompleteOnTask)
     );
